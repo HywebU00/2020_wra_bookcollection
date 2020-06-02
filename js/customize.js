22407b7951ac469f02fdcc6c5267c9980006b9c8
anniewu <annie@hyweb.com.tw>
2020-06-02 12:23:22 +0800

    20200602修改上傳

--- js/customize.js
+++ js/customize.js
@@ -66,25 +66,25 @@ $(function() {
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 1,
+                    slidesToScroll: 4,
                 }
             }, {
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 1,
+                    slidesToScroll: 4,
                 }
             }, {
                 breakpoint: 767,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1,
+                    slidesToScroll: 3,
                 }
             }, {
                 breakpoint: 575,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
+                    slidesToScroll: 2,
                 }
             }, {
                 breakpoint: 480,
@@ -243,31 +243,31 @@ $(function() {
         infinite: true,
         speed: 300,
         slidesToShow: 6,
-        slidesToScroll: 1,
+        slidesToScroll: 6,
         responsive: [{
                 breakpoint: 1200,
                 settings: {
                     slidesToShow: 5,
-                    slidesToScroll: 1,
+                    slidesToScroll: 5,
                     
                 }
             }, {
                 breakpoint:991,
                 settings: {
                     slidesToShow: 4,
-                    slidesToScroll: 1
+                    slidesToScroll: 4
                 }
             }, {
                 breakpoint:767,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1
+                    slidesToScroll: 3
                 }
             }, {
                 breakpoint:575,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1
+                    slidesToScroll: 2
                 }
             }, {
                 breakpoint:480,
