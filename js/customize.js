1c8a804910d556b57927eca5d6f252bf615a06fb
anniewu <annie@hyweb.com.tw>
2022-04-28 14:26:06 +0800

    20220428上傳檔案

--- js/customize.js
+++ js/customize.js
@@ -858,3 +858,23 @@ $(function() {
         $('.theme_area_piclist').show();
     })
 })
+$(function() {
+    // password_toggle
+    var passShow = false;
+    $('.password_toggle').each(function(index, el) {
+        $(this).find('.btn-icon').off().click(function(e) {
+            if (!passShow) {
+                $(this).children('i').removeClass().addClass('i_show');
+                $(this).parents('.password_toggle').find('input[type="password"]').attr('type', 'text');
+                passShow = true;
+                // console.log(passShow);
+            } else {
+                $(this).children('i').removeClass().addClass('i_hide');
+                $(this).parents('.password_toggle').find('input[type="text"]').attr('type', 'password');
+                passShow = false;
+                // console.log(passShow);
+            }
+            e.preventDefault();
+        });
+    });
+})
